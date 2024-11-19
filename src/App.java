58cf8c070258f04da1fa14faa0f83881d7b6db4c
AlteaTaulet <alteataulet@gmail.com>
2024-11-19 11:39:32 +0100

    2. Defining the main

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome!!");
         System.out.println("Stuff Happens...");
+        System.out.println("More stuff happens");
     }
 }
