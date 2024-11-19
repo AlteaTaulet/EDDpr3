b2e612e1c2b4d41515b0021e8128b45360430602
AlteaTaulet <alteataulet@gmail.com>
2024-11-19 11:47:17 +0100

    3. Definiendo el contador

--- src/App.java
+++ src/App.java
@@ -3,5 +3,13 @@ public class App {
         System.out.println("Welcome!!");
         System.out.println("Stuff Happens...");
         System.out.println("More stuff happens");
+
+        contador();
+    }
+
+    private static void contador() {
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
     }
 }
